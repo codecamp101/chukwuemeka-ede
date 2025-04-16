1ac9466e051c991a55aac114d4d9c71aea670095
codecamp101 <phildca114@gmail.com>
2025-04-16 14:05:19 +0100

    responsive design changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const fm = document.querySelector('form');
 const pop = document.querySelector('#pop');
 const user = document.querySelector('#user');
 const start = document.querySelector('#start');
-const pan = document.querySelector('#panel')
+const pan = document.querySelector('#panel');
 //this unmasks the password
 eye.addEventListener('mousedown', () => {
     ipt.type = 'text';
@@ -27,6 +27,7 @@ fm.addEventListener('submit',(e) => {
     const pd= 'chedm3';
     if (pd == ipt.value){
         user.remove();
+        
         fm.remove();
         output.textContent = 'signing you in...';
         const timeoutID = setTimeout(() => {
@@ -43,3 +44,33 @@ fm.addEventListener('submit',(e) => {
 start.addEventListener('click', () => {
     pan.classList.toggle('on');
 })
+//this formats a unix number into a human-readable date
+const dob = new Date('06/28/2014'); //date format in JavaScript mm-dd-yyyy
+pan.querySelector('#dob').textContent = Intl.DateTimeFormat('en-us', {dateStyle: 'full'}).format(dob);
+
+//functions
+// A function is a group of statements that does not run unless when called
+function calculator () {
+    console.log(2 + 4);
+    console.log(10 - 5);
+    console.log(10 / 2);
+    console.log(3 * 3);
+    console.log(5 % 3); //modulus (short form is mod): the r
+}   
+calculator()
+function callConfetti () {
+    /*
+    const t = "ME";
+    const u = "4";
+    console.log(t - u);//concastenation: join strings together
+    */
+   const d = new Date("06/28/2014").getDate();
+   const m = new Date("06/28/2014").getMonth();
+   const today = Date.now();
+   console.log(m);
+   console.log(new Date(today).getDate())
+   if (new Date(today).getDate() === d && new Date(today).getMonth() === m) {
+    pan.classList.add('bgi');
+   }
+}
+callConfetti();
